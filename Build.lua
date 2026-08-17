01d15b60e153b5bfe8fa39257e4122765f44a189
Silent Comet <Ayushmaan123456@outlook.com>
2026-08-17 20:52:05 +0530

    Initial check-in: singularity engine set-up

---
+++ Build.lua
@@ -0,0 +1,14 @@
+workspace "Singularity"
+    architecture "x64"
+    configurations { "Debug", "Release" }
+    startproject "App"
+
+    output_bin = "%{wks.location}/Build/output"
+    output_int = "%{wks.location}/Build/intermediate/%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}/%{prj.name}"
+
+    filter "system:windows"
+        systemversion "latest"
+
+-- Include sub-folders scripts
+include "core/Core.lua"
+include "App/App.lua"
